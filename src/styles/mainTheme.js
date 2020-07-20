20e34c67ff1d091cc497fa200a55c590deef4a08
Aryan Agarwal <aryankush025@gmail.com>
2020-07-21 05:04:35 +0530

    new theme colors added

--- src/styles/mainTheme.js
+++ src/styles/mainTheme.js
@@ -1,11 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import lightBlue from '@material-ui/core/colors/lightBlue';
 
-// Checkout https://material-ui.com/customization/default-theme/
-// To customize theme
+// Checkout https://material-ui.com/customization/default-theme/ to customize theme
+// Checkout https://material-ui.com/customization/color/ to customize theme color
 
 const mainTheme = createMuiTheme({
   palette: {
-    type: 'dark'
+    type: 'light',
+    primary: blue,
+    secondary: lightBlue
   }
 });
 
